a62a33e45ce1f65f42e0ae4da75cb679765b315f
James Pritts <James.Pritts@gmail.com>
2022-04-04 01:41:35 -0500

    Adding actual files

---
+++ Deflate.ps1
@@ -0,0 +1,75 @@
+# 3D Viewer:
+Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage
+# Calculator:
+Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+# 3D Builder:
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+# Calendar and Mail:
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+# Alarms and Clock:
+Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+# Camera:
+Get-AppxPackage *windowscamera* | Remove-AppxPackage
+# Uninstall Get Help:
+Get-AppxPackage *Microsoft.GetHelp* -AllUsers | Remove-AppxPackage
+# Get Office:
+Get-AppxPackage *officehub* | Remove-AppxPackage
+# Get Started:
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+# Get Skype:
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+# Groove Music:
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+# Maps:
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+# Microsoft Solitaire Collection:
+# Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+# Money:
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+# Movies & TV:
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+# News:
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+# OneNote:
+Get-AppxPackage *onenote* | Remove-AppxPackage
+# People:
+Get-AppxPackage *people* | Remove-AppxPackage
+# Phone Companion:
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+# Photos:
+Get-AppxPackage *photos* | Remove-AppxPackage
+# Sports:
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+# Store:
+Get-AppxPackage *windowsstore* | Remove-AppxPackage
+# Voice Recorder:
+# Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+# Weather:
+# Get-AppxPackage *bingweather* | Remove-AppxPackage
+
+# Xbox:
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
+Get-ProvisionedAppxPackage -Online | `
+Where-Object { $_.PackageName -match "xbox" } | `
+ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $_.PackageName }
+
+Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco update chocolatey -y
+
+choco install packages.config -y
+
+#Create new folder on deksktop called "Program Manager"
+mkdir $HOME/Desktop/Program Manager
+
+#Move all shortcuts on desktop to folder called "Program Manager"
+Move-Item C:/Users/Public/Desktop/*.lnk "$HOME/Desktop/Program Manager"
+
+#Run ccleaner from application folder
+C:\Program Files (x86)\CCleaner\CCleaner.exe
+
+#Open gmail.com
+Start-Process chrome https://www.gmail.com
